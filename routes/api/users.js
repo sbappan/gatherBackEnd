76e6ac7bb111f8a6c14be4839f30aa5b810a9538
Santhosh B. Appan <12586088+sbappan@users.noreply.github.com>
2020-07-16 22:44:41 -0400

    Hotfix: Missing pswd during login, included now

--- routes/api/users.js
+++ routes/api/users.js
@@ -63,40 +63,42 @@ router.post('/login', (req, res) => {
   const { email } = req.body;
   const { password } = req.body;
   // Find user by email
-  User.findOne({ email }).then(user => {
-    // Check if user exists
-    if (!user) {
-      return res.status(404).json({ message: 'Email not found' });
-    }
-    // Check password
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        // User matched
-        // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name,
-        };
-        // Sign token
-        jwt.sign(
-          payload,
-          process.env.SECRET_KEY,
-          {
-            expiresIn: 31556926, // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              message: 'Login successful',
-              token,
-            });
-          }
-        );
-      } else {
-        return res.status(400).json({ message: 'Password incorrect' });
+  User.findOne({ email })
+    .select('+password')
+    .then(user => {
+      // Check if user exists
+      if (!user) {
+        return res.status(404).json({ message: 'Email not found' });
       }
+      // Check password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User matched
+          // Create JWT Payload
+          const payload = {
+            id: user.id,
+            name: user.name,
+          };
+          // Sign token
+          jwt.sign(
+            payload,
+            process.env.SECRET_KEY,
+            {
+              expiresIn: 31556926, // 1 year in seconds
+            },
+            (err, token) => {
+              res.json({
+                success: true,
+                message: 'Login successful',
+                token,
+              });
+            }
+          );
+        } else {
+          return res.status(400).json({ message: 'Password incorrect' });
+        }
+      });
     });
-  });
 });
 
 router.get('/', (req, res) => {
